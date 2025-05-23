4b5215cf310fbfce212015d56d4692b7b283f39a
Olesya Orlenko <olees.orlenko@yandex.ru>
2025-05-23 15:41:21 -0700

    Add ProfileViewController

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet var photoImage: UIImageView!
+    
+    @IBOutlet var nameLabel: UILabel!
+    
+    @IBOutlet var nicknameLabel: UILabel!
+    
+    @IBOutlet var textLabel: UILabel!
+    
+    @IBOutlet var logoutButton: UIButton!
+    
+    @IBAction private func didTapLogoutButton() {
+    }
+}
