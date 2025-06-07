ecf2c4b8adab3fd9cea765cc4f4639b1833f1d22
Olesya Orlenko <olees.orlenko@yandex.ru>
2025-06-06 20:28:39 -0700

    Remove Constants.swift from repository

--- ImageFeed/Constants.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-enum Constants {
-    static let accessKey = "CzbkiPeYfnZqSSLaqNHnPfZsYMavhNoEsryPJncAPkQ"
-    static let secretKey = "igS5XIJcZjHf1kvdzNvOd3_v6IxgM1mmq49VbdkTJqk"
-    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
-    static let accessScope = "public+read_user+write_likes"
-    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
-}
